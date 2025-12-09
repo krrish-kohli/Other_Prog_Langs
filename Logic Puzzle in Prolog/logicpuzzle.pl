c95ce418e1309742a5a5348d7f4c40155e60b933
krrish-kohli <krrishkohli@icloud.com>
2025-12-08 17:34:35 -0800

    files modified prolog

--- Logic Puzzle in Prolog/logicpuzzle.pl
+++ Logic Puzzle in Prolog/logicpuzzle.pl
@@ -1,3 +1,5 @@
+% "Star Tricked Solution Template.pl"
+%
 % CECS 342 Assignment 3
 % Logic Puzzle
 % Solution Template: >>> Please fill in the missing code! <<<
@@ -11,27 +13,6 @@
 earlier(A, B) :-
   append(Earlier, Later, ['Tuesday', 'Wednesday', 'Thursday', 'Friday']),
   member(A, Earlier), member(B, Later), !.
-
-% Frisbee person cannot be Ms. Gort
-not_gort('Mr. Klaatu').
-not_gort('Ms. Barada').
-not_gort('Mr. Nikto').
-
-% Nikto can only be Wed, Thu, or Fri
-nikto_day('Wednesday').
-nikto_day('Thursday').
-nikto_day('Friday').
-
-% Klaatu's object cannot be the water tower
-klaatu_object(balloon).
-klaatu_object(frisbee).
-klaatu_object(clothesline).
-
-% Friday must be Ms. Barada OR clothesline
-friday_ok(Events) :-
-    member(('Ms. Barada', _, 'Friday'), Events).
-friday_ok(Events) :-
-    member((_, clothesline, 'Friday'), Events).
   
 % Solve the puzzle.
 solve :-
@@ -57,26 +38,33 @@ solve :-
   % 1. Mr. Klatu made his sighting at some point earlier in the week
   %    than the one who saw the balloon, but at some point later in the week
   %    than the one who spotted the Frisbee (who isn't Ms. Gort).
+  
+    member((FrisbeePerson, frisbee, FrisbeeDay), Events),
+    \+ FrisbeePerson = 'Ms. Gort',
+    earlier(FrisbeeDay, KlaatuDay),
+    earlier(KlaatuDay, BalloonDay),
 
-  member((FrisbeePerson, frisbee, FrisbeeDay), Events),
-  earlier(KlaatuDay, BalloonDay),
-  earlier(FrisbeeDay, KlaatuDay),
-  not_gort(FrisbeePerson),
       
   % 2. Friday's sighting was made by either Ms. Barada
   %    or the one who saw the clothesline or both.
-
-  friday_ok(Events),      
+  
+    member((FridayPerson, FridayObject, 'Friday'), Events),
+    ( FridayPerson = 'Ms. Barada'
+    ; FridayObject = clothesline
+    ),
+      
       
   % 3. Mr. Nikto did not make his sighting on Tuesday.
   
-  member(('Mr. Nikto', _, NiktoDay), Events),
-  nikto_day(NiktoDay),
+    member(('Mr. Nikto', _, NiktoDay), Events),
+    \+ NiktoDay = 'Tuesday',
+      
       
   % 4. Mr. Klatu isn't the one whose object turned out to be a water tower.
   
-  member(('Mr. Klaatu', KlaatuObject, KlaatuDay), Events),
-  klaatu_object(KlaatuObject),      
+    member(('Mr. Klaatu', KlaatuObject, _), Events),
+    \+ KlaatuObject = watertower,
+      
       
   % Display a solution: An English sentence for each event.
   forall(member(T, Events), report(T)).
@@ -85,3 +73,4 @@ solve :-
 report((Who, What, When)) :-
   write(Who), write(' saw the '), write(What),
   write(' on '), write(When), write('.'), nl.
+
